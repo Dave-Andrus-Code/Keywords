ec600b66bf6244bcfcbe8c377cfc4b2aff8c9143
code@da-ac.com <code@da-ac.com>
2022-10-23 19:32:50 -0500

    Initial version of Keywords

---
+++ Keyword.py
@@ -0,0 +1,93 @@
+# KEYWORDS
+# Written by DMA on 2022-10-23
+# A simple string keyword extraction algorythm that summarizes the content.
+# Originally intended to summarize job descriptions with similar job titles
+# so I know what is most import aspects.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# Imports
+import os, argparse
+import operator
+import numpy as np
+import scipy as stats
+from collections import OrderedDict
+from utils import *
+
+
+# Globals
+txtlines = {}   # All the text lines in a dictionary
+keywords = {}   # All key works in a dictionary
+
+# Prep
+cmd = argparse.ArgumentParser(description='Parses keywords from a text file.')
+cmd.add_argument('-f', dest='file', help='The path and filename to summarize.')
+args = cmd.parse_args()
+
+# Get the file
+fname = args.file
+if not fname:
+    fname = os.path.join('.', 'sample', 'sample_data_eng_mgr.txt')
+    print('No filename specified; using the sample file: ', fname)
+
+try:
+    f = open(fname, 'r', encoding='utf8')
+except:
+    print('ERROR:  Unable to open input file.')
+    raise SystemError
+
+# Read the file and 
+for lines in f:
+    line = strstrip(f.readline()).strip()
+    
+    # make a dictionary of unique lines
+    if line not in txtlines.keys():
+        # lines with 3 words or less are typically headers in a hierachy and not useful here
+        if line.strip().count(' ') > 3:
+            txtlines[line] = {}
+    
+    # make a dictionary of unique keywords
+    for s in  line.split(' '):
+        ls = s.lower()
+        # check for plurals
+        if ls[-1:] == 's' and ls not in snouns:
+            ls = ls[0:len(ls)-1]
+        # add the keyboard if it's not ignored, blank, or only 1 character
+        if ls not in ignore and ls != '' and len(ls) > 1:
+            if ls not in keywords.keys():
+                keywords[ls] = []
+            if line not in keywords[ls]:
+                keywords[ls].append(line)
+
+# Count the keywords
+kcnt = {}
+for x in keywords:
+    kcnt[x] = len(keywords[x])
+
+# Sort the dictionary
+sorted_tuples = sorted(kcnt.items(), key=operator.itemgetter(1), reverse=True)
+sk = OrderedDict()
+for k, v in sorted_tuples:
+    sk[k] = v
+
+
+# Calculate the significance
+arr = list(sk.values())
+smean = np.mean(arr)
+sserr = np.std(arr)
+npsig = normal_dist(arr, smean, sserr)
+sig = npsig.tolist()
+
+# output
+c = 0
+for x in sk:
+    if sk[x] > 1:
+        print ('KEYWORD:  ', x, ' (' + str(sk[x]) + ' / ' + str(round(100-sig[c], 2)) + '%)')
+        for z in keywords[x]:
+            print ('  *', z)
+        print()
+    c=c+1
+
